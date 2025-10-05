1e181934c08c304bc2e06c835f2debf888586a7f
Mpho <mphonzibane@gmail.com>
2025-10-05 13:42:44 +0200

    Create WeatherApiService for OpenWeatherMap API

---
+++ app/src/main/java/vcmsa/projects/travelapp/api/WeatherApiService.kt
@@ -0,0 +1,23 @@
+package vcmsa.projects.travelapp.api
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+import vcmsa.projects.travelapp.data.model.WeatherResponse
+
+interface WeatherApiService {
+    
+    @GET("weather")
+    suspend fun getCurrentWeather(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): WeatherResponse
+    
+    @GET("weather")
+    suspend fun getWeatherByCity(
+        @Query("q") cityName: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric"
+    ): WeatherResponse
+}
